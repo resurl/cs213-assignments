93284443df926203fd4b00c7a19ca5d66df7c477
Leanna <leanna.resurreccion@gmail.com>
2020-02-29 02:30:59 -0800

    added comments to asm

--- a6/q3.s
+++ a6/q3.s
@@ -3,9 +3,9 @@ ld $s, r0           # r0 = &s
 ld (r0), r1         # r1 = s 
 ld $n, r2
 ld (r2), r2         # r2 = n
-ld $0, r3
+ld $0, r3           # i for increment
 
-avgloop: mov r3, r4          # temp i
+avgloop: mov r3, r4 # temp i
 not r4
 inc r4
 add r2, r4          # n-i
@@ -14,7 +14,7 @@ mov r2, r3          # reset i (i = n)
 dec r3              # i = n-1 (for sort)
 br sort             # else goto sort
 
-avg: ld $0, r6
+avg: ld $0, r6      # r6 = 0
      ld 4(r1), r5   # s[i].grade[0]
      add r5, r6
      ld 8(r1), r5   # s[i].grade[1]
@@ -23,18 +23,18 @@ avg: ld $0, r6
      add r5, r6
      ld 16(r1), r5  # s[i].grade[3]
      add r5, r6
-     shr $2, r6
+     shr $2, r6     # r6 = grade sum / 4
      st r6, 20(r1)  # s[i].average = grade >> 2
-     ld $24, r6
+     ld $24, r6     # r6 = 24 (byte offset)
      add r6, r1     # next student
-     inc r3
-     br avgloop
+     inc r3         # i++ 
+     br avgloop     # calculate avg of s[i]
 
-sort: ld (r0), r2 # &s[0]
-      ld $1, r5    # j
-      ld (r0), r1  # reset s 
-      bgt r3, inner
-      br median
+sort: ld (r0), r2   # &s[0]
+      ld $1, r5     # j
+      ld (r0), r1   # reset s 
+      bgt r3, inner # if i > 0, goto inner (loop)
+      br median     # else get student id w median avg
 
 inner: mov r3, r4   # i for comparison
        not r4
@@ -42,72 +42,72 @@ inner: mov r3, r4   # i for comparison
        add r5, r4   # j - i
        ld $24, r1   # r1 = 24
        add r1, r2   # &s[j]
-       bgt r4, sort  
-       br sortif
+       bgt r4, sort # if j > i goto sort (outer loop)
+       br sortif    # else (j <= i) goto sortif (if statement)
 
-sortif: ld 20(r2), r6 # r6 = s[j].average
-        ld $24, r7
+sortif: ld 20(r2), r6   # r6 = s[j].average
+        ld $24, r7      # r7 = 24 for offset
         not r7
         inc r7
-        add r7, r2
-        ld 20(r2), r7 #r7 = s[j-1]
+        add r7, r2      # r7 = &s[j-1]
+        ld 20(r2), r7   # r7 = s[j-1].average
         not r7
-        inc r7
-        add r7, r6 #r6 = a[j-1].average - s[j].average
-        ld $24, r7
-        add r7, r2
-        bgt r6, body
-        inc r5
-        br inner
+        inc r7          # r7 = -s[j-1].average
+        add r7, r6      # r6 = s[j-1].average - s[j].average ?
+        ld $24, r7      
+        add r7, r2      # move s[j-1] pointer up
+        bgt r6, body    # if (s[j-1 average > s[j].average) goto body (inside if)
+        inc r5          # else j++ 
+        br inner        # goto inner (loop, beginning)
 
-body: ld $24, r7
+body: ld $24, r7        
       not r7
       inc r7
       add r7, r2
-      ld 24(r2), r1 #r1 = s[j].sid
-      ld 0(r2), r7 #r7 = s[j-1].sid
-      st r1, 0(r2) #s[j-1].sid = s[j].sid
-      st r7, 24(r2)    #s[j].sid = s[j-1].sid
-      ld 28(r2), r1 #r1 = s[j].sid
-      ld 4(r2), r7 #r7 = s[j-1].sid
-      st r1, 4(r2) #s[j-1].sid = s[j].sid
-      st r7, 28(r2)    #s[j].sid = s[j-1].sid
-      ld 32(r2), r1 #r1 = s[j].sid
-      ld 8(r2), r7 #r7 = s[j-1].sid
-      st r1, 8(r2) #s[j-1].sid = s[j].sid
-      st r7, 32(r2)    #s[j].sid = s[j-1].sid
-      ld 36(r2), r1 #r1 = s[j].sid
-      ld 12(r2), r7 #r7 = s[j-1].sid
-      st r1, 12(r2) #s[j-1].sid = s[j].sid
-      st r7, 36(r2)    #s[j].sid = s[j-1].sid
-      ld 40(r2), r1 #r1 = s[j].sid
-      ld 16(r2), r7 #r7 = s[j-1].sid
-      st r1, 16(r2) #s[j-1].sid = s[j].sid
-      st r7, 40(r2)    #s[j].sid = s[j-1].sid
-      ld 44(r2), r1 #r1 = s[j].sid
-      ld 20(r2), r7 #r7 = s[j-1].sid
-      st r1, 20(r2) #s[j-1].sid = s[j].sid
-      st r7, 44(r2)    #s[j].sid = s[j-1].sid
+      ld 24(r2), r1     #r1 = s[j].sid
+      ld 0(r2), r7      #r7 = s[j-1].sid
+      st r1, 0(r2)      #s[j-1].sid = s[j].sid
+      st r7, 24(r2)     #s[j].sid = s[j-1].sid
+      ld 28(r2), r1     #r1 = s[j].sid
+      ld 4(r2), r7      #r7 = s[j-1].sid
+      st r1, 4(r2)      #s[j-1].sid = s[j].sid
+      st r7, 28(r2)     #s[j].sid = s[j-1].sid
+      ld 32(r2), r1     #r1 = s[j].sid
+      ld 8(r2), r7      #r7 = s[j-1].sid
+      st r1, 8(r2)      #s[j-1].sid = s[j].sid
+      st r7, 32(r2)     #s[j].sid = s[j-1].sid
+      ld 36(r2), r1     #r1 = s[j].sid
+      ld 12(r2), r7     #r7 = s[j-1].sid
+      st r1, 12(r2)     #s[j-1].sid = s[j].sid
+      st r7, 36(r2)     #s[j].sid = s[j-1].sid
+      ld 40(r2), r1     #r1 = s[j].sid
+      ld 16(r2), r7     #r7 = s[j-1].sid
+      st r1, 16(r2)     #s[j-1].sid = s[j].sid
+      st r7, 40(r2)     #s[j].sid = s[j-1].sid
+      ld 44(r2), r1     #r1 = s[j].sid
+      ld 20(r2), r7     # r7 = s[j-1].sid
+      st r1, 20(r2)     #s[j-1].sid = s[j].sid
+      st r7, 44(r2)     #s[j].sid = s[j-1].sid
       ld $24, r7
       add r7, r2
       inc r5
       br inner
 
-median: ld $n, r2
-        ld (r2), r2
-        inc r2
-        shr $1, r2
-        dec r2
-        mov r2, r6
-        mov r2, r7
-        shl $3, r6
-        shl $4, r7
+median: ld $n, r2       
+        ld (r2), r2     # r2 = n
+        inc r2          
+        shr $1, r2      # r2 = (n + 1) / 2
+        dec r2          # ((n + 1) / 2) - 1
+        mov r2, r6      # r6 = middle 
+        mov r2, r7      # r7 = middle
+        shl $3, r6      # r6 = middle*8
+        shl $4, r7      # r7 = middle*16
         add r6, r2
-        add r7, r2 #r2 *= 24
-        ld (r0), r5
-        ld (r5, r2, 4), r4
+        add r7, r2      # r2 *= 24
+        ld (r0), r5     # &s[0]
+        ld (r5, r2, 4), r4 #s[middle]
         ld $m, r3
-        st r4, (r3)
+        st r4, (r3)     # m = s[middle].sid
 
 halt
 
